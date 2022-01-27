333420172de807581aaeb3258e55160914b9fea6
ziggieallen <93275742+ziggieallen@users.noreply.github.com>
2022-01-27 09:23:36 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+//PART ONE
+
+class Vehicle {
+    constructor(make,model,year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        return 'Beep';
+    }
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}`;
+    }
+}
+
+// PART TWO
+
+class Car extends Vehicle {
+    constructor(make,model,year) {
+        super(make,model,year);
+        this.numWheels = 4;
+    }
+}
+
+// PART THREE
+
+class Motorcycle extends Vehicle {
+    constructor(make,model,year) {
+        super(make,model,year);
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return "VROOM!!!";
+    }
+}
+
+// PART FOUR
+
+class Garage {
+    constructor(capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+    add(newVehicle) {
+        if (!(newVehicle instanceof Vehicle)) {
+            return "Only vehicles are allowed here";
+        }
+        if (this.vehicles.length >= this.capacity) {
+            return "Sorry, we're full";
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!"
+    }
+}
